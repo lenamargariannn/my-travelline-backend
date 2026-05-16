90e6aa745f8bf1d5850275e4939eae2700a39db3
Lena Margaryan <lena.margaryan.2001@gmail.com>
2026-05-16 20:50:02 +0400

    feat: add ContactMessageRepository

---
+++ src/main/java/com/mytravelline/contact/ContactMessageRepository.java
@@ -0,0 +1,16 @@
+package com.mytravelline.contact;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
+
+    Page<ContactMessage> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    Page<ContactMessage> findByReadFalseOrderByCreatedAtDesc(Pageable pageable);
+
+    long countByReadFalse();
+}

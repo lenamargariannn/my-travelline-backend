1fd32459bcfefd9d3f64c6969858947f84bd0d2b
Lena Margaryan <lena.margaryan.2001@gmail.com>
2026-05-16 20:44:10 +0400

    feat: add AdminUserRepository

---
+++ src/main/java/com/mytravelline/admin/AdminUserRepository.java
@@ -0,0 +1,15 @@
+package com.mytravelline.admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AdminUserRepository extends JpaRepository<AdminUser, UUID> {
+
+    Optional<AdminUser> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
